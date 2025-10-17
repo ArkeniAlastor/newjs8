7563a4283a242a592f9b45a86d19d892d335b6df
zuboda48 <zuboda48@29-3>
2025-10-17 15:03:41 +0300

    ff

---
+++ main.js
@@ -0,0 +1,34 @@
+let Stive_text = "Невозможно соединить точки, глядя в будущее; их можно соединить, только оглядываясь назад. Поэтому нужно верить, что точки каким-то образом соединятся в будущем"
+
+let St_photo = document.querySelector("#photo1 img")
+St_photo.src = "/img/St.png"
+
+let St_about = document.getElementById("about1")
+St_about.innerHTML = Stive_text
+
+let St_Name = document.getElementById("name1")
+St_Name.innerHTML = "Стив Джобс"
+
+
+let Elon_text = "Неудача — это один из вариантов. Если вы не терпите неудачу, значит, вы недостаточно внедряете инновации"
+
+let Em_photo = document.querySelector("#photo2 img")
+Em_photo.src = "/img/Em.png"
+
+let Em_about = document.getElementById("about2")
+Em_about.innerHTML = Elon_text
+
+let Em_Name = document.getElementById("name2")
+Em_Name.innerHTML = "Илон Маск"
+
+
+let Pavel_text = "Чтобы быть по-настоящему свободным, нужно быть готовым пойти на любой риск ради свободы"
+
+let _photo = document.querySelector("#photo3 img")
+_photo.src = "/img/Pd.jpg"
+
+let _about = document.getElementById("about3")
+_about.innerHTML = Pavel_text
+
+let _Name = document.getElementById("name3")
+_Name.innerHTML = "Павел Дуров"
